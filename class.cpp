1fa0a502b35c44f6c7f00d61d43e9d38c4a551c7
MadanKS14 <madanks1413@gmail.com>
2025-07-26 22:41:12 +0530

    Refactor code structure for improved readability and maintainability

--- class.cpp
+++ class.cpp
@@ -1,53 +1,25 @@
 #include<iostream>
 using namespace std;
 
-class Bank {
+class Area{
 
-  string acc_name;
-  int acc_num;
-  int amount;
-  static int total;
-  static int total_balance;
   public:
-  Bank(string acc_name,int acc_num, int amount){
-    this->acc_name=acc_name;
-    this->acc_num=acc_num;
-    this->amount=amount;
-    total++;
-    total_balance+=amount;
+  int calculate(int a){
+    return 3.14*a*a;
   }
 
-  static void staticAccess(){
-    cout<<total<<endl;
-    cout<<total_balance<<endl;
+  int calculate(int a,int b,int c){
+    return a*b*c;
   }
 
 
-  void Deposit(int amt){
-    amount+=amt;
-    total_balance+=amt;
-  }
-
-  void Disply(){
-    cout<<acc_name<<" "<<acc_num<<" "<<amount<<endl;
-  }
-
- 
 };
 
-int Bank::total=0;
-int Bank::total_balance=0;
-
 int main(){
-  Bank b1("Madan K S",1, 5000);
-  Bank b2("Pavan K S",2 ,7000);
-  b1.Disply();
-  b2.Disply();
-  Bank::staticAccess();
-  b1.Deposit(1000);
-  b2.Deposit(1000);
-  Bank::staticAccess();
 
 
-  return 0;
+Area a;
+cout<<a.calculate(5);
+cout<<a.calculate(5,5,5);
+return 0;
 }
