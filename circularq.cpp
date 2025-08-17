65d3dc2dca6d73bbd15ccd0c91af6b551f64449a
MadanKS14 <madanks1413@gmail.com>
2025-08-17 21:26:53 +0530

    Implement circular queue class with push, pop, and front methods

---
+++ circularq.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class CQ {
+    int *arr;
+    int cap, currsize, front, rear;
+public:
+    CQ(int n) {
+        cap = n;
+        arr = new int[cap];
+        currsize = 0;
+        front = 0;
+        rear = -1;
+    }
+
+    void push(int val) {
+        if (currsize == cap) {
+            cout << "Queue is full" << endl;
+            return;
+        }
+        rear = (rear + 1) % cap;
+        arr[rear] = val;
+        currsize++;
+    }
+
+    void pop() {
+        if (currsize == 0) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        front = (front + 1) % cap;
+        currsize--;
+    }
+
+    int Front() {
+        if (currsize == 0) {
+            cout << "Queue is empty" << endl;
+            return -1;
+        }
+        return arr[front];
+    }
+
+};
+
+int main() {
+    CQ q(3);
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4); // Should print "Queue is full"
+
+    cout << q.Front() << endl; // Should print 1
+
+    q.pop();
+    cout << q.Front() << endl; // Should print 2
+
+    q.pop();
+    q.pop();
+    q.pop(); // Should print "Queue is empty"
+
+    return 0;
+}
