c55f350ccb54b4ed6eae517d063a1fd8d83afbb1
MadanKS14 <madanks1413@gmail.com>
2025-11-16 22:44:26 +0530

    Refactor binary search implementation and restore sorting algorithms

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -7,22 +7,29 @@ int bsearch(vector<int> &nums,int target){
   int start=0;
   int end=n-1;
   while(start<=end){
-    int mid = (start+end)/2;
-    if(target>nums[mid]){
-      start=mid+1;
+    int mid = start+(end-start)/2;
+    if(target==nums[mid]){
+      return mid;
     }
     else if(target<nums[mid]){
       end=mid-1;
     }
     else{
-      return mid;
+      start=mid+1;
     }
   }
+  return -1;
 }
 
 int main(){
   vector<int> nums={1,4,7,8,13,45,67,89,99};
-  int target= 99;
-  cout<<"key found at :"<<bsearch(nums,target);
+  int target= 7;
+  int idx= bsearch(nums,target);
+  if( idx!= -1){
+    cout<<"key found at :"<<idx<<endl;
+  }else{
+    cout<<"key not found"<< endl;
+  }
+  
   return 0;
 }
