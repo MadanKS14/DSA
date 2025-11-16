ea45658cdf86a35fef44406b37d21090bada34d0
MadanKS14 <madanks1413@gmail.com>
2025-11-16 22:45:11 +0530

    Restore sorting algorithms and remove commented-out code

--- sorting.cpp
+++ sorting.cpp
@@ -1,206 +1,206 @@
-// #include <iostream>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// void bubblesort(vector<int> &arr)
-// {
-//   int n = arr.size();
+void bubblesort(vector<int> &arr)
+{
+  int n = arr.size();
 
-//   for (int i= 0; i < n - 1; i++)
-//   {
-//     for (int j=0; j < n - i - 1; j++)
-//     {
-//       if (arr[j] > arr[j + 1])
-//       {
-//         int temp = arr[j];
-//         arr[j] = arr[j + 1];
-//         arr[j + 1] = temp;
-//       }
-//     }
-//   }
-// }
+  for (int i= 0; i < n - 1; i++)
+  {
+    for (int j=0; j < n - i - 1; j++)
+    {
+      if (arr[j] > arr[j + 1])
+      {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
 
-// int main()
-// {
-//   vector<int> arr = {5, 3, 8, 4, 2};
-//   int n=arr.size();
+int main()
+{
+  vector<int> arr = {5, 3, 8, 4, 2};
+  int n=arr.size();
 
-//   cout << "before sorting :" << endl;
-//   for (int i = 0; i < n; i++)
-//   {
-//     cout << arr[i] << " ";
-//   }
+  cout << "before sorting :" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
 
-//   bubblesort(arr);
+  bubblesort(arr);
 
-//   cout << "before sorting :" << endl;
-//   for (int i = 0; i < n; i++)
-//   {
-//     cout << arr[i] <<" ";
-//   }
+  cout << "before sorting :" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] <<" ";
+  }
 
-//   return 0;
-// }
+  return 0;
+}
 
-// #include <iostream>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// void selectionsort(vector<int> &arr)
-// {
-//   int n = arr.size();
+void selectionsort(vector<int> &arr)
+{
+  int n = arr.size();
 
-//   for(int i=0;i<n;i++){
-//     int min=i;
-//     for(int j=i+1;j<n;j++){
-//       if(arr[j]<arr[min]){
-//         min=j;
-//       }
-//     }
-//     swap(arr[i],arr[min]);
-//   }
+  for(int i=0;i<n;i++){
+    int min=i;
+    for(int j=i+1;j<n;j++){
+      if(arr[j]<arr[min]){
+        min=j;
+      }
+    }
+    swap(arr[i],arr[min]);
+  }
 
-// }
+}
 
-// int main()
-// {
-//   vector<int> arr = {5, 3, 8, 4, 2};
-//   int n=arr.size();
+int main()
+{
+  vector<int> arr = {5, 3, 8, 4, 2};
+  int n=arr.size();
 
-//   cout << "before sorting :" << endl;
-//   for (int i = 0; i < n; i++)
-//   {
-//     cout << arr[i] << " ";
-//   }
+  cout << "before sorting :" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
 
-//   selectionsort(arr);
+  selectionsort(arr);
 
-//   cout << "before sorting :" << endl;
-//   for (int i = 0; i < n; i++)
-//   {
-//     cout << arr[i] <<" ";
-//   }
+  cout << "before sorting :" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] <<" ";
+  }
 
-//   return 0;
-// }
+  return 0;
+}
 
-// #include <iostream>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// void insertionsort(vector<int> &arr)
-// {
-//   int n = arr.size();
+void insertionsort(vector<int> &arr)
+{
+  int n = arr.size();
 
-//   for (int i = 1; i < n; i++)
-//   {
-//     int j = i - 1;
-//     int key = arr[i];
+  for (int i = 1; i < n; i++)
+  {
+    int j = i - 1;
+    int key = arr[i];
 
-//     while (j >= 0 && arr[j] > key)
-//     {
-//       arr[j + 1] = arr[j];
-//       j--;
-//     }
-//     arr[j + 1] = key;
-//   }
-// }
+    while (j >= 0 && arr[j] > key)
+    {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+}
 
-// int main()
-// {
-//   vector<int> arr = {5, 3, 8, 4, 2};
-//   int n = arr.size();
+int main()
+{
+  vector<int> arr = {5, 3, 8, 4, 2};
+  int n = arr.size();
 
-//   cout << "before sorting :" << endl;
-//   for (int i = 0; i < n; i++)
-//   {
-//     cout << arr[i] << " ";
-//   }
+  cout << "before sorting :" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
 
-//   insertionsort(arr);
+  insertionsort(arr);
 
-//   cout << "before sorting :" << endl;
-//   for (int i = 0; i < n; i++)
-//   {
-//     cout << arr[i] << " ";
-//   }
+  cout << "before sorting :" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
 
-//   return 0;
-// }
+  return 0;
+}
 
-// #include <iostream>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-// void merge(vector<int> &arr, int st, int mid, int end)
-// {
-//   int i = st;
-//   int j = mid + 1;
-//   vector<int> temp;
-//   while (i <= mid && j <= end)
-//   {
-//     if (arr[i] < arr[j])
-//     {
-//       temp.push_back(arr[i]);
-//       i++;
-//     }
-//     else
-//     {
-//       temp.push_back(arr[j]);
-//       j++;
-//     }
-//   }
+void merge(vector<int> &arr, int st, int mid, int end)
+{
+  int i = st;
+  int j = mid + 1;
+  vector<int> temp;
+  while (i <= mid && j <= end)
+  {
+    if (arr[i] < arr[j])
+    {
+      temp.push_back(arr[i]);
+      i++;
+    }
+    else
+    {
+      temp.push_back(arr[j]);
+      j++;
+    }
+  }
 
-//   while (i <= mid)
-//   {
-//     temp.push_back(arr[i]);
-//     i++;
-//   }
-//   while (j <= end)
-//   {
-//     temp.push_back(arr[j]);
-//     j++;
-//   }
+  while (i <= mid)
+  {
+    temp.push_back(arr[i]);
+    i++;
+  }
+  while (j <= end)
+  {
+    temp.push_back(arr[j]);
+    j++;
+  }
 
-//   for (int idx = 0; idx < temp.size(); idx++)
-//   {
-//     arr[st + idx] = temp[idx];
-//   }
-// }
+  for (int idx = 0; idx < temp.size(); idx++)
+  {
+    arr[st + idx] = temp[idx];
+  }
+}
 
-// void mergesort(vector<int> &arr, int st, int end)
-// {
-//   if (st < end)
-//   {
-//     int mid = st + (end - st) / 2;
-//     mergesort(arr, st, mid);
-//     mergesort(arr, mid + 1, end);
-//     merge(arr, st, mid, end);
-//   }
-// }
+void mergesort(vector<int> &arr, int st, int end)
+{
+  if (st < end)
+  {
+    int mid = st + (end - st) / 2;
+    mergesort(arr, st, mid);
+    mergesort(arr, mid + 1, end);
+    merge(arr, st, mid, end);
+  }
+}
 
-// int main()
-// {
-//   vector<int> arr = {5, 3, 8, 4, 2};
-//   int n = arr.size();
+int main()
+{
+  vector<int> arr = {5, 3, 8, 4, 2};
+  int n = arr.size();
 
-//   cout << "before sorting :" << endl;
-//   for (int i = 0; i < n; i++)
-//   {
-//     cout << arr[i] << " ";
-//   }
+  cout << "before sorting :" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
 
-//   cout << " " << endl;
-//   mergesort(arr, 0, arr.size() - 1);
+  cout << " " << endl;
+  mergesort(arr, 0, arr.size() - 1);
 
-//   cout << "after sorting :" << endl;
-//   for (int i = 0; i < n; i++)
-//   {
-//     cout << arr[i] << " ";
-//   }
+  cout << "after sorting :" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
 
-//   return 0;
-// }
+  return 0;
+}
 
 #include <iostream>
 #include <vector>
