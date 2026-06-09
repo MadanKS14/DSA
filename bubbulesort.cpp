d9945ecd88d4bb6bb16b63db23ec84cf9c3f9cb4
MadanKS14 <madanks1413@gmail.com>
2026-06-09 21:22:33 +0530

    Remove unnecessary blank lines for improved code clarity in bubblesort function

--- bubbulesort.cpp
+++ bubbulesort.cpp
@@ -6,7 +6,6 @@ void bubblesort(int a[], int n)
   for (int i = 0; i < n - 1; i++)
   {
     bool isSwap = false;
-
     for (int j = 0; j < n - i - 1; j++)
     {
       if (a[j] > a[j + 1])
@@ -15,7 +14,6 @@ void bubblesort(int a[], int n)
         isSwap = true;
       }
     }
-
     if (!isSwap)
       return;
   }
