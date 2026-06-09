682304534972d53c9d553bc1297d7120326ac22e
MadanKS14 <madanks1413@gmail.com>
2026-06-09 21:22:04 +0530

    Refactor bubblesort function for improved readability and consistency

--- bubbulesort.cpp
+++ bubbulesort.cpp
@@ -1,31 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void bubblesort(int a[],int n){
-  bool isSwap;
-  for(int i=0;i<n-1;i++){
-      bool isSwap=false;
-    for(int j=0;j<n-i-1;j++){
-      if(a[j]>a[j+1]){
-        swap(a[j],a[j+1]);
-        isSwap=true;
+void bubblesort(int a[], int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    bool isSwap = false;
+
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      if (a[j] > a[j + 1])
+      {
+        swap(a[j], a[j + 1]);
+        isSwap = true;
       }
     }
-   
+
+    if (!isSwap)
+      return;
   }
-   if(!isSwap) return;
 }
 
-void printarray(int a[],int n){
-  for(int i=0;i<n;i++){
-    cout<<a[i]<<" ";
+void printarray(int a[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << a[i] << " ";
   }
-  
 }
 
-int main(){
-  int a[]={3,5,7,1,2};
-  int n=5;
-  bubblesort(a,n);
-  printarray(a,n);
+int main()
+{
+  int a[] = {3, 5, 7, 1, 2};
+  int n = 5;
+  bubblesort(a, n);
+  printarray(a, n);
 }
